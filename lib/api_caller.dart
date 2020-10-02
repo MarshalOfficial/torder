223b67e107b405645226d2bda05a4edb48ec78d1
MohadmadReza <mr.rahmatzade371@gmail.com>
2020-10-02 20:19:23 +0330

    fix and feat api caller and api repo

---
+++ lib/api_caller.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'SecretLoader.dart';
+import 'Secret.dart';
+
+class ApiCaller {
+  static Future<List<dynamic>> callWithHeaserParams(
+      String procName, String params) async {
+    //params must be a json with key values of parameters that backend needed
+    Secret secret = await SecretLoader().load();
+    String finalUrl =
+        '${secret.apiurl}/?procname=' + procName + '&params=' + params;
+
+    final response = await http.post(finalUrl);
+    if (response.statusCode == 200) {
+      var standardJson =
+          json.encode(json.decode(utf8.decode(response.bodyBytes)));
+      var data = json.decode(standardJson);
+      List<dynamic> test = json.decode(data);
+      return test;
+    } else {
+      return null;
+    }
+  }
+
+  static Future<List<dynamic>> callWithBodyParams(String body) async {
+    //body must be a standard json
+    Secret secret = await SecretLoader().load();
+    String finalUrl = '${secret.apiurlbody}';
+
+    final response = await http.post(finalUrl);
+
+    if (response.statusCode == 200) {
+      var standardJson =
+          json.encode(json.decode(utf8.decode(response.bodyBytes)));
+      var data = json.decode(standardJson);
+      List<dynamic> test = json.decode(data);
+      return test;
+    } else {
+      return null;
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:uuid/uuid.dart';
-import './api_repo.dart';
 
 void main() => runApp(MyApp());
 
